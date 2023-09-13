63d6eda9aa65af2e647a7641ae30d32fedb81ac2
AJIT KUMAR PANDIT <ajitkumarpandit1999@gmail.com>
2023-09-13 20:02:31 +0530

    v 1.1

---
+++ credit-debit-card/src/components/Body/Body.jsx
@@ -0,0 +1,19 @@
+// src/App.js
+import "./Body.css";
+
+function Body() {
+  return (
+    <>
+      <div className="bodyContainer">
+        <div className="leftSidePart">
+
+        </div>
+        <div className="rightSidePart">
+            
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Body;
