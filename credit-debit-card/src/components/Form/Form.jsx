525ecf152c0f6b183308451219ca78986f41f572
AJIT KUMAR PANDIT <ajitkumarpandit1999@gmail.com>
2023-09-14 14:45:09 +0530

    v 1.2.1.1

--- credit-debit-card/src/components/Form/Form.jsx
+++ credit-debit-card/src/components/Form/Form.jsx
@@ -2,36 +2,14 @@ import React, { useState } from 'react';
 import './Form.css';
 
 function Form() {
-//   const [name, setName] = useState('');
-//   const [email, setEmail] = useState('');
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     // Handle form submission logic here
-//   };
+ 
 
   return (
-    <div className='FormCenter'>
-      <form>
-        <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-          />
+    <>
+      <form className='FormCenter'>
         
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-        
-        <button type="submit">Submit</button>
       </form>
-    </div>
+    </>
   );
 }
 
